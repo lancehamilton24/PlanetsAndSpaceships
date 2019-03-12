9843262a84bb7bbb2d76b1e2246fc251a5282ef0
Lance Hamilton <lance.hamilton24@gmail.com>
2019-03-11 19:29:56 -0500

    Add project files.

---
+++ OuterSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OuterSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
